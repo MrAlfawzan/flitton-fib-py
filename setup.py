81ea6d8d1e842700a0b2f7030b9d8775becdcb23
MrAlfawzan <bin.fawzan@gmail.com>
2023-03-14 15:34:12 +0000

    flitton_fib_py/__init__.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name="flitton_fib_py",
+    version="0.0.1",
+    author="Bin Fawzan Flitton",
+    author_email="bin.fawzan@gmail.com",
+    description="Calculates a Fibonacci number",
+    long_description="A basic library that \
+      calculates Fibonacci numbers",
+    long_description_content_type="text/markdown",
+    url="https://github.com/MrAlfawzan/flitton-fib-py",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    tests_require=['pytest'],
+)
